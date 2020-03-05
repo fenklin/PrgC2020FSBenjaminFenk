f265654ded0e2716681ad13a62de7490f4f34497
Benjamin Fenk <benjamin.fenk@stud.hslu.ch>
2020-03-05 20:30:02 +0100

    initial upload

---
+++ 00_Exercises/02b_multiTableDesc/multiTableDesc.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void PrintUsageMessage(void);  //function prototype
+void PrintMultiTable( int );
+
+const int lowerLimit = 1;
+const int upperLimit = 20;
+
+int main( int argc, char * argv[])
+{
+	int inputValue = 0;
+	if(argc != 2) //not equal 2
+	{
+		PrintUsageMessage();
+		return 0;
+	}
+	else
+	{
+		inputValue = atoi(argv[1]);
+		if( inputValue >= lowerLimit && inputValue <= upperLimit)
+		{
+			//input is valid
+			PrintMultiTable( inputValue ); 
+		}
+		else
+		{
+			//no valid input
+			PrintUsageMessage();
+			return 0;
+		}
+	}
+	return 0;
+
+}
+
+
+
+
+void PrintUsageMessage(void)
+{
+	printf("Usage: multiTable wholeNumber\n\t \
+wholeNumber: integer on the interval [%d,%d]\n", lowerLimit, upperLimit);
+}
+
+
+void PrintMultiTable( int tableLimit )
+{
+	for( int i = tableLimit; i >= 1; i--)
+	{
+		for(int j = tableLimit; j >= 1; j--)
+		{
+			printf("%d\t", i*j);
+		}
+		printf("\n");
+	}
+}
